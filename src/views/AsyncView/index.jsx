205074e95e68caa056bb4d55b3e43598582fd181
leon <jianliangzhang@ctrip.com>
2017-12-19 18:35:32 +0800

    上传文件

--- src/views/AsyncView/index.jsx
+++ src/views/AsyncView/index.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Upload, message, Button, Icon } from 'antd'
 
-const AsyncView = () => (
-  <p>
-    I'm Async View.
-  </p>
-)
+class AsyncView extends React.Component{
+  constructor(props) {
+    super(props)
+  }
+
+  uploadProps = {
+    name: 'file',
+    action: '/api/user/uploadAvatar/',
+    headers: {
+      authorization: 'authorization-text',
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    }
+  }
+
+  render() {
+    return (
+      <Upload {...this.uploadProps}>
+        <Button>
+          <Icon type="upload" /> Click to Upload
+        </Button>
+      </Upload>
+    )
+  }
+}
 
 export default AsyncView
